11be0d6ac204a41c99cc20efc8a7d712cd606882
lzcwds <duyu_zz@163.com>
2018-08-28 16:31:47 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Login from '@/view/login'
+import Layout from '@/view/layout'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },{
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: 'dashboard',
+      component: Layout,
+      children: [
+        {
+          path: 'dashboard',
+          component: () => import('@/view/dashboard'),
+          name: 'Dashboard',
+          meta: { title: 'dashboard', icon: 'dashboard', noCache: true }
+        }
+      ]
     }
   ]
 })
