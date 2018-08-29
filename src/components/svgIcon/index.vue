194571334c789f843b585e0368ea5217450cecc8
lzcwds <duyu_zz@163.com>
2018-08-29 13:54:06 +0800

    add icon and reset css

---
+++ src/components/svgIcon/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <svg :class="svgClass" aria-hidden="true">
+        <use :xlink:href="iconName"/>
+    </svg>
+</template>
+
+<script>
+    export default {
+        prop:{
+          iconClass:{
+              type:String,
+              required:true
+          },
+          className:{
+              type:String,
+              default:''
+          }
+        },
+        name: 'SvgIcon',
+        computed: {
+            iconName() {
+                return `#icon-${this.iconClass}`
+            },
+            svgClass() {
+                if (this.className) {
+                    return 'svg-icon ' + this.className
+                } else {
+                    return 'svg-icon'
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import './icons'
+
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
